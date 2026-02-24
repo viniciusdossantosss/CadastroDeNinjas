5104eef4beb75fd2cd557316ae76ef29ac47ae1f
Vinicius dos Santos <viniciusreis.pro@gmail.com>
2026-02-24 12:02:07 -0300

    Adição do Controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Boas vindas";
+    }
+
+}
