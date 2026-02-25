0705fcfd7a641aaa79b3fb9a3c52df85a19c3e7b
VINICIUS DOS SANTOS REIS <014.381672@universidadesenaicimatec.edu.br>
2026-02-25 15:29:34 -0300

    'fix' : Coloca a coluna de email como unique = true

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -20,6 +20,7 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
     private String email;
     private int idade;
     // @ManyToOne um ninja tem uma unica missão
